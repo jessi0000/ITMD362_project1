1b641e1feeed6fd0693d6441f6d3acb6c838c365
JinmingHuang <jhuang96@hawk.iit.edu>
2024-10-10 20:43:43 -0500

    ITMD362_project1

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Thank you for signing up!');
+    // Here, you could extend this to include more complex behaviors like AJAX submissions
+});
